392e15383962fc8cc5396c9a43cac4f974ba8dd7
WayneJr <blairu92@gmail.com>
2020-03-18 19:06:19 +0000

    thejsp

---
+++ src/java/com/wayne/GetSms.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wayne;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author root
+ */
+public class GetSms extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        response.setContentType("text/html;charset=UTF-8");
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet GetSms</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet GetSms at " + request.getContextPath() + "</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
+//    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    // The fields that will be necessary for twilio authentication
+    public static final String ACCOUNT_SID = "AC71e67635f40f73e901aca66f60bcd467";
+    public static final String AUTH_TOKEN = "5fcbb445c3d4bf28b32613a22fd0c473";
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+        
+        String herAnswer = request.getParameter("answer");
+        // The part of the code that will send the sms
+        // Initialize the twilio
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        
+        // Compile and send the message
+        Message message = Message.creator(new PhoneNumber("+2348082090998"),
+        new PhoneNumber("+16475589945"), herAnswer).create();
+
+        try (PrintWriter out = response.getWriter()) {
+            out.println("Response Successfully sent!");
+            RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+            
+        }
+        System.out.println(message.getSid());
+    }
+    
+    
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,26 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="com.twilio.Twilio" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--<link rel="stylesheet" href="css/main.css">-->
+    <link href="webapp/resources/static/css/main.css" rel="stylesheet" type="text/css"/>
+    <title>Be Mine Baby</title>
+</head>
+<body>
+    <div class="all">
+        <!-- <h1>What will you do?</h1> -->
+        <h1>Be Mine, Will You? </h1>
+    
+        <div class="formdiv">
+            <form action="herAnswer.jsp" method="POST">
+                <input type="text" placeholder="Yes or Yes" name="answer" id="answer"> <br>
+                <input type="button" class="answerButton" value="OK">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
